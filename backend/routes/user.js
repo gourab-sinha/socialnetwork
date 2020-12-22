4d2fb5d09bca5cfadec61244f9e665a95cdef973
gourab-sinha <gourab19964u@gmail.com>
2020-12-22 15:47:14 +0530

    signup issues fixed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,7 +7,7 @@ router.post("/signup", (req,res,next) => {
     bcrypt.hash(req.body.password, 10).then(hash =>{
         const user = new User({
             email: req.body.email,
-            password: req.body.password
+            password: hash
         });
         user.save().then(result => {
             res.status(201).json({
